6b1647b76aa2a7bfdbb54ec6b18e5a920d139caa
William <gcphost@gmail.com>
2018-03-22 13:14:52 -0700

    better load out

--- kernel.js
+++ kernel.js
@@ -3,11 +3,18 @@
 const fold = require('adonis-fold');
 const path = require('path');
 const app = require('./config/app');
+const Helpers = require('lesswork-framework/Helpers');
 
 const packageFile = require(path.join(__dirname, '/package.json'));
 
 module.exports = function (callback, providers) {
   providers = providers || app.providers;
+
+  fold.Ioc.singleton('Lesswork/Helpers', function (app) {
+    return new Helpers(path.join(__dirname));
+  });
+
+  fold.Ioc.alias('Adonis/Src/Helpers', 'Lesswork/Helpers');
 
   fold.Registrar
     .register(providers)
@@ -19,8 +26,6 @@ module.exports = function (callback, providers) {
           fold.Ioc.autoload(load, path.join(__dirname, packageFile.autoload[load]));
         }
       }
-
-      use('Helpers').load(__dirname);
 
       use('App').registerGlobals();
 
