effff097d21027cf8a8ba75ba17b199415816a8c
William <gcphost@gmail.com>
2018-03-27 00:13:37 -0700

    env update, laravel config/app, rescue folder for translations

--- bootstrap/functions.js
+++ bootstrap/functions.js
@@ -1,3 +1,5 @@
- module.exports = function () {
-   return require('lesswork-framework/lib/functions')(__dirname, require('../config/app').functions);
- };
+require('lesswork-framework/lib/env');
+
+module.exports = function () {
+  return require('lesswork-framework/lib/functions')(__dirname, require('../config/app').functions);
+};
--- config/app.js
+++ config/app.js
@@ -1,82 +1,166 @@
 'use strict';
 
-/*
-|--------------------------------------------------------------------------
-| Application Providers
-|--------------------------------------------------------------------------
-|
-| Here we configure the providers required to run lesswork application. They
-| are registered only once and can be used inside any file using `use`
-| keyword.
-|
-*/
-const providers = [
-  'lesswork-framework/Providers/AppProvider',
-  'lesswork-framework/Providers/RequestProvider',
-  'lesswork-framework/Providers/ResponseProvider',
-  'lesswork-framework/Providers/EventProvider',
-];
-
-/*
-|--------------------------------------------------------------------------
-| Namespace Aliases
-|--------------------------------------------------------------------------
-|
-| Each provider is registered with a long unique namespace. Here we alias
-| them with a short unique name to keep our import statements short and
-| sweet.
-|
-*/
-const aliases = {
-  App: 'Lesswork/App',
-  Response: 'Lesswork/Response',
-  Config: 'Lesswork/Config',
-  Helpers: 'Lesswork/Helpers',
-  Env: 'Lesswork/Env',
-  Event: 'Lesswork/Event',
-  Request: 'Lesswork/Request',
-  State: 'Lesswork/State',
-  Exception: 'Lesswork/Exception',
-  Route: 'Lesswork/Route',
-  ErrorTransformer: 'lesswork-framework/Transformers/ErrorTransformer',
-};
-
-/*
-|--------------------------------------------------------------------------
-| Work Providers
-|--------------------------------------------------------------------------
-|
-| Work providers are specific to work, and are not registered when running routes. It helps in reducing boot time.
-|
-*/
-const workProviders = [
-  //
-];
-
-/*
-|--------------------------------------------------------------------------
-| Work Commands
-|--------------------------------------------------------------------------
-|
-| Work Commands are also are registered inside the IoC container. Here we
-| register with Work Kernel using their unique namespace.
-|
-*/
-const commands = {
-  //'great': './app/Console/Commands/GreetCommand',
-};
-
-// serverless functions to be autoloaded.
-const functions = [
-  'Http/Routes',
-  'Http/Authentication',
-  'Http/Functions',
-];
-
 module.exports = {
-  providers,
-  aliases,
-  workProviders,
-  commands,
-  functions
+  /*
+  |--------------------------------------------------------------------------
+  | Application Environment
+  |--------------------------------------------------------------------------
+  |
+  | This value determines the "environment" your application is currently
+  | running in. This may determine how you prefer to configure various
+  | services your application utilizes. Set this in your ".env.js" file.
+  |
+  */
+
+  env: env('APP_ENV', 'production'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Application Debug Mode
+  |--------------------------------------------------------------------------
+  |
+  | When your application is in debug mode, detailed error messages with
+  | stack traces will be shown on every error that occurs within your
+  | application. If disabled, a simple generic error page is shown.
+  |
+  */
+
+  debug: env('APP_DEBUG', false),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Application Timezone
+  |--------------------------------------------------------------------------
+  |
+  | Here you may specify the default timezone for your application, which
+  | will be used by the PHP date and date-time functions. We have gone
+  | ahead and set this to a sensible default for you out of the box.
+  |
+  */
+
+  timezone: env('APP_TIMEZONE', 'UTC'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Application Locale Configuration
+  |--------------------------------------------------------------------------
+  |
+  | The application locale determines the default locale that will be used
+  | by the translation service provider. You are free to set this value
+  | to any of the locales which will be supported by the application.
+  |
+  */
+
+  locale: env('APP_LOCALE'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Application Fallback Locale
+  |--------------------------------------------------------------------------
+  |
+  | The fallback locale determines the locale to use when the current one
+  | is not available. You may change the value to correspond to any of
+  | the language folders that are provided through your application.
+  |
+  */
+
+  fallback_locale: env('APP_LOCALE'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Encryption Key
+  |--------------------------------------------------------------------------
+  |
+  | This key is used by the Illuminate encrypter service and should be set
+  | to a random, 32 character string, otherwise these encrypted strings
+  | will not be safe. Please do this before deploying an application!
+  |
+  */
+
+  key: env('APP_KEY'),
+
+  cipher: 'AES-256-CBC',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Autoloaded Service Providers
+  |--------------------------------------------------------------------------
+  |
+  | The service providers listed here will be automatically loaded on the
+  | request to your application. Feel free to add your own services to
+  | this array to grant expanded functionality to your applications.
+  |
+  */
+  providers: [
+    'lesswork-framework/Providers/AppProvider',
+    'lesswork-framework/Providers/RequestProvider',
+    'lesswork-framework/Providers/ResponseProvider',
+    'lesswork-framework/Providers/EventProvider',
+    'lesswork-framework/Providers/TranslationProvider',
+  ],
+
+  /*
+  |--------------------------------------------------------------------------
+  | Class Aliases
+  |--------------------------------------------------------------------------
+  |
+  | This array of class aliases will be registered when this application
+  | is started. However, feel free to register as many as you wish as
+  | the aliases are "lazy" loaded so they don't hinder performance.
+  |
+  */
+  aliases: {
+    App: 'Lesswork/App',
+    Response: 'Lesswork/Response',
+    Config: 'Lesswork/Config',
+    Helpers: 'Lesswork/Helpers',
+    Env: 'Lesswork/Env',
+    Event: 'Lesswork/Event',
+    Request: 'Lesswork/Request',
+    State: 'Lesswork/State',
+    Exception: 'Lesswork/Exception',
+    Route: 'Lesswork/Route',
+    ErrorTransformer: 'lesswork-framework/Transformers/ErrorTransformer',
+    Translation: 'Lesswork/Translation',
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Work Providers
+  |--------------------------------------------------------------------------
+  |
+  | Work providers are specific to work, and are not registered when running routes. It helps in reducing boot time.
+  |
+  */
+  workProviders: [
+    //
+  ],
+
+  /*
+  |--------------------------------------------------------------------------
+  | Work Commands
+  |--------------------------------------------------------------------------
+  |
+  | Work Commands are also are registered inside the IoC container. Here we
+  | register with Work Kernel using their unique namespace.
+  |
+  */
+  commands: {
+    //'great': './app/Console/Commands/GreetCommand',
+  },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Serverless Autoload Functions
+  |--------------------------------------------------------------------------
+  |
+  | The following paths will be automatically loaded as functions
+  | in your serverless configuration.
+  |
+  */
+  functions: [
+    'Http/Routes',
+    'Http/Authentication',
+    'Http/Functions',
+  ],
 };
