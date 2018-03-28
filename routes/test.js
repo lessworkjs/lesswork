ec3b75a911ecd74931d78f39b72f904c2d9150a1
William <gcphost@gmail.com>
2018-03-28 00:01:38 -0700

    oops

--- routes/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Route = require('lesswork-framework/src/Route');
-
-module.exports = {
-  get: function () {
-    return Route(arguments).get('helloWorld', 'App/Http/Controllers/testController@get');
-  },
-};
